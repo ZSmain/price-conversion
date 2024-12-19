058109399e5d79469d6e66bed069b2dfd1f6e91a
Ismail <ishmael.zouaoui@gmail.com>
2024-12-19 15:14:32 +0100

    Add admin interface for ExchangeRate and Conversion models

--- price_conversion/admin.py
+++ price_conversion/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Conversion, ExchangeRate
+
+
+@admin.register(ExchangeRate)
+class ExchangeRateAdmin(admin.ModelAdmin):
+    list_display = ("currency", "rate", "date")
+
+
+@admin.register(Conversion)
+class ConversionAdmin(admin.ModelAdmin):
+    list_display = (
+        "from_price",
+        "from_currency",
+        "from_unit",
+        "to_price",
+        "to_currency",
+        "to_unit",
+    )
